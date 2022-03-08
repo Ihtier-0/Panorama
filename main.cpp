3865d851c7dc400b92bdd6c6dd5a914de091da2c
ihtier <ihtier_@outlook.com>
2022-03-08 15:55:29 +0300

    - из main удалены части которые больше не будут поддерживаться

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 #include <QCoreApplication>
 
+#include <QElapsedTimer>
 #include <QPainter>
-
-#include "ImageCombiner.h"
-#include "testUtils/testUtils.h"
 
 #include "BRIEF/BRIEF.h"
 #include "FAST/FAST.h"
@@ -14,8 +12,6 @@
 #include "utils/utils.h"
 
 int main(int argc, char *argv[]) {
-  //  startTests(argc, argv);
-
   QCoreApplication a(argc, argv);
 
   QImage left("rio-00.png");
@@ -167,15 +163,5 @@ int main(int argc, char *argv[]) {
     bothImage.save("bestSimilarBothImage.jpg");
   }
   qDebug() << "DONE!" << timer.elapsed() << "ms";
-
-  //  QImage right("right.jpg");
-  //  QImage left("left.jpg");
-  //  // делается чтобы видеть результат на монохромных картинках
-  //  right.convertTo(QImage::Format_RGB888);
-  //  left.convertTo(QImage::Format_RGB888);
-
-  //  ImageCombiner combiner;
-  //  combiner.combine(left, right);
-
   return 0;
 }
